fc1aa14efad5df7ea94822ad0908b6b345f4802b
sahabuddin <sahabuddinriyaj984@gmail.com>
2023-03-21 02:16:33 +0600

    Create Component Tree

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import './App.css';
-
+import MainComponent from './components/mainComponent';
 function App() {
   return (
     <div className="App">
-      
+      <MainComponent />
     </div>
   );
 }
