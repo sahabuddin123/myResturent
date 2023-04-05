8ff0fbba51080e802ba1e11ae75bf545cc1a6390
sahabuddin <sahabuddinriyaj984@gmail.com>
2023-04-05 19:57:45 +0600

    fix router redirect issus

--- src/components/body/Body.js
+++ src/components/body/Body.js
@@ -3,15 +3,19 @@ import Menu from './Menu';
 import Home from './Home';
 import About from './About';
 import Contact from './Contact';
-import { Route,Redirect } from 'react-router';
+import { Route, Redirect, Switch } from 'react-router';
 const Body = () => {
-    return(
+    return (
         <div>
             {/* <Menu /> */}
-            <Route path="/" exact component={Home}/>
-            <Route path="/menu" exact component={Menu}/>
-            <Route path="/contact" exact component={Contact}/>
-            <Route path="/about" exact component={About}/>
+            <Switch>
+                <Route path="/home" exact component={Home} />
+                <Route path="/menu" exact component={Menu} />
+                <Route path="/contact" exact component={Contact} />
+                <Route path="/about" exact component={About} />
+                <Redirect from='/' to="/home" />
+            </Switch>
+
         </div>
     );
 }
